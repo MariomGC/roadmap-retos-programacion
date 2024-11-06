bbede25cc7007558991ceacc83f6e309ac7fe705
MariomGC <mariom.gomez.c@gmail.com>
2024-11-06 15:23:11 -0600

    Update Javidp01.py
    Ejemplo de la solución.

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Javidp01.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Javidp01.py
@@ -10,3 +10,41 @@ omentar en varias líneas
 '''
 my_variable = "hola"
 my_variable = "adiós"
+
+
+#Paso 1; seleccioanr un Lenguaje y escribir la página web https://www.python.org/
+
+#Paso 2: Emplear diferentes sintaxis que existen de crear comentarios en el lenguaje.
+""" Esta es una manera de escribir un comentario, entre comilas dobles
+que puedes emplear en varios rengloes; siempre y cuando inicie apropiadamente
+con triple comilla y cierre con triple comilla."""
+
+'Esta es otra manera de escribir un comentario, iniciando y terminado con  comillas simples'
+
+#También así
+#se puede escribir
+#comentarios
+#aunque es menos productivo
+#con un hashtag al inicio de cada línea de código.
+
+"""RESUMEN:
+Usa # para comentarios de una sola línea.
+Usa varios # o cadenas de texto con triple comilla para comentarios de varias líneas.
+Usa tres comillas "para documentar funciones, clases o módulos."""
+
+#Paso 3: Crea una variable (y una constante si el lenguaje lo soporta).
+My_variable_Edad: 35 #Variable
+my_variable_Altura: 1.70 #Variable
+
+PI: float= 3.1415 #Constante
+
+MY_CONSTANT= "Constante que no cambia"
+
+#Paso 4:
+my_int: 1
+my_float: 1.5
+my_bool: True
+variable_de_texto: str = "Esta es una cadena de texto"
+
+
+print ("¡Hola, Python!")
